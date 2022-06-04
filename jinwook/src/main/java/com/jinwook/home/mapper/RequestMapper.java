4bda79068fdf38c99dcbfdda19891bc0d3d6fdc2
ghdtj <ghdtj@LAPTOP-OVMUIC5C>
2022-06-04 16:53:22 +0900

    Hong 2022.06.04 request 목록만 남음

--- jinwook/src/main/java/com/jinwook/home/mapper/RequestMapper.java
+++ jinwook/src/main/java/com/jinwook/home/mapper/RequestMapper.java
@@ -12,7 +12,7 @@ public interface RequestMapper {
 
 	public int addRequestAddStore(Store store);  					// 상점 예비 등록 
 	
-	public Store getRequestStore(String userId);					// 상점 등록 요청 보기
+	public Request getRequestStore(int reqNo);						// 상점 등록 요청 보기
 	
 	public int updateRequestAddStore(int storeNo);					// 상점 등록 수락
 	
@@ -20,10 +20,21 @@ public interface RequestMapper {
 
 	public int updateRequestRefund(String userId);					// 환급 요청 수락
 
+	public int addRequestDeleteStore(Request request);				// 상점 삭제 신청
 	
+	public Request countPickup (int storeNo);					// 남은 픽업 준비중 개수
 	
+	public int deleteStore(Request request);						// 상점 삭제 수락
 	
-	public int updateRequestStatusToAccept(Request request);		// 요청 상태 변경 (수락)
+	public int addRequestAd(Request request);						// 광고 등록 신청
+	
+	public Request getRequestAd(int reqNo);							// 광고 등록 신청 보기
+	
+	public int updateRequestStatusToAccept(int reqNo);				// 요청 상태 변경 (수락)
+	
+	public int updateRequestStatusToRefuse(int reqNo);				// 요청 상태 변경 (거절)
+	
+	public int deleteRequest(int reqNo);							// 요청 목록 삭제 (상태변경)
 		
 	public Store checkStoreStatus(int storeNo);  					// ***삭제예정*** (상점 등록 수락 확인을 위한 method)
 
