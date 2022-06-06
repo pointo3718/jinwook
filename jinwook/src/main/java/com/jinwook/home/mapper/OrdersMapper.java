6f8a5541c73449f88ae43d68669d9da8d6cd544a
rkdal <rkdal@DESKTOP-DNSLD67>
2022-06-06 22:10:23 +0900

    Kang 2022.06.06 저녁 종집합

--- jinwook/src/main/java/com/jinwook/home/mapper/OrdersMapper.java
+++ jinwook/src/main/java/com/jinwook/home/mapper/OrdersMapper.java
@@ -32,7 +32,7 @@ public interface OrdersMapper {
 		
 	public int updateOrdersCart(Cart cart);			//장바구니 수정
 		
-	public int deleteOrderCartAfter(Cart cart);		//장바구니	삭제(status변경)
+	public int deleteOrdersCartAfter(Cart cart);		//장바구니	삭제(status변경)
 	
 	public int deleteOrdersCart(int cartNo);			//장바구니 목록삭제
 	
@@ -54,16 +54,16 @@ public interface OrdersMapper {
 	
 	public int getOrdersJpaylistTotalCount();		//진욱페이 내역카운트
 	
-	public int addOrdersNotice(Notice notice);
+	public int addOrdersNotice(Notice notice);		//알림등록
 	
-	public int deleteOrdersNotice(int notiNo);
+	public int deleteOrdersNotice(int notiNo);		//알림삭제
 	
-	public List<Notice>getOrdersNoticelist(String receiveId);
+	public List<Notice>getOrdersNoticelist(String receiveId); 	//알림목록
 
-	public int getOrdersNoticelistTotalCount();
+	public int getOrdersNoticelistTotalCount();		//알림목록개수
 	
-	public int updateOrdersCeoJb(Jpay jpay);
+	public int updateOrdersCeoJb(Jpay jpay);		//사장님 jb증가
 	
-	public int updateOrdersCeoEarn(Store store);
+	public int updateOrdersCeoEarn(Store store);	//사장님 매출액 증가
 }
  
--- jinwook/src/main/java/com/jinwook/home/service/orders/OrdersService.java
+++ jinwook/src/main/java/com/jinwook/home/service/orders/OrdersService.java
@@ -19,35 +19,29 @@ public int addOrders(Orders orders);
 	
 	public int deleteOrders(int orderNo);			
 	
-	public int updateOrders(Orders orders);			//주문내역(수락,거절,픽업완료)
+	public int updateOrders(Orders orders);					
 	
-	public int getOrdersTotalCount();				//주문목록 개수
-	
-	public int addOrdersCart(Cart cart);			//장바구니 담기
+	public int addOrdersCart(Cart cart);			
 		
-	public int updateOrdersCart(Cart cart);			//장바구니 수정
+	public int updateOrdersCart(Cart cart);			
 		
-	public int deleteOrderCartAfter(Cart cart);		//장바구니	삭제(status변경)
+	public int deleteOrdersCartAfter(Cart cart);		
 	
-	public int deleteOrdersCart(int cartNo);			//장바구니 목록삭제
+	public int deleteOrdersCart(int cartNo);			
 	
-	public int deleteOrdersCartAll(boolean cartStatus);		//장바구니 목록전부삭제
+	public int deleteOrdersCartAll(boolean cartStatus);		
 	
-	public List<Cart> getOrdersCartList(Cart cart);	//장바구니 목록
-
-	public int getCartTotalCount();					//장바구니 품목개수
+	public List<Cart> getOrdersCartList(Cart cart);				
 	
-	public int addOrdersJpayPassword(User user);	//진욱페이 비밀번호 등록
+	public int addOrdersJpayPassword(User user);	
 	
-	public int updateOrdersJpayPassword(User user); //진욱페이 비밀번호 수정
+	public int updateOrdersJpayPassword(User user); 
 	
-	public int addOrdersJpayCharge(Jpay jpay);		//진욱페이 충전
+	public int addOrdersJpayCharge(Jpay jpay);		
 	
-	public int addOrdersjBCharge(Object object);	//진욱페이 잔액 
+	public int addOrdersjBCharge(Object object);	
 	
-	public List<Jpay> getOrdersJpaylist(String userId);			//진욱페이 내역
-	
-	public int getOrdersJpaylistTotalCount();		//진욱페이 내역카운트
+	public List<Jpay> getOrdersJpaylist(String userId);				
 	
 	public int addOrdersNotice(Notice notice);
 	
@@ -55,8 +49,6 @@ public int addOrders(Orders orders);
 	
 	public List<Notice>getOrdersNoticelist(String receiveId);
 
-	public int getOrdersNoticelistTotalCount();
-	
 	public int updateOrdersCeoJb(Jpay jpay);
 	
 	public int updateOrdersCeoEarn(Store store);
